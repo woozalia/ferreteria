95517960383ad095f21e6d48b3760dd6f62c599e
root <root@rizzo-3>
2015-09-20 10:41:59 -0400

    mostly just tidying/reorganizing (tree.php)

--- util/tree.php
+++ util/tree.php
@@ -17,93 +17,38 @@ class clsTreeNode {
     protected $strName;
     protected $intRefs;
 
-    // INFORMATIONAL
+    // ++ SETUP ++ //
+
+    public function Spawn() {
+	$sClass = get_class($this);
+	//$obj = new clsTreeNode();
+	$obj = new $sClass();
+	return $obj;
+    }
+
+    // -- SETUP -- //
+    // ++ DATA FIELDS ++ //
+
     public function Name($iName=NULL) {
 	if (!is_null($iName)) {
 	    $this->strName = $iName;
 	}
 	return $this->strName;
     }
+    public function Value($iVal=NULL) {
+	if (!is_null($iVal)) {
+	    if (!is_string($iVal)) {
+		echo 'Trying to set node value using a <b>'.get_class($iVal).'</b> object instead of a string.<br>';
+		throw new exception('Internal error');
+	    }
+	    $this->vVal = $iVal;
+	}
+	return $this->vVal;
+    }
 
-    // SUBNODE basic functions
-    public function Nodes(array $iNodes=NULL) {
-	if (is_array($iNodes)) {
-	    $this->arSubs = $iNodes;
+    // -- DATA FIELDS -- //
+    // ++ PARENT NODE ++ //
 
-	    foreach ($iNodes as $name => $node) {
-		$node->Parent($this);
-	    }
-	}
-	return $this->arSubs;
-    }
-    /*----
-      ACTION: Retrieve the first sub-node.
-      PURPOSE: Mainly useful when there is exactly one
-	sub-node which we expect to exist.
-    */
-    public function FirstNode() {
-	$oNode = reset($this->arSubs);	// get the first node
-	return $oNode;			// return it
-    }
-    public function Node($iName, clsTreeNode $iNode=NULL) {
-	if (!is_null($iNode)) {
-	    $iNode->Name($iName);
-	    $this->NodeAdd($iNode);
-	}
-	if (isset($this->arSubs[$iName])) {
-	    return $this->arSubs[$iName];
-	} else {
-	    return NULL;
-	}
-    }
-    /*----
-      ACTION: searches from the current node downward to find
-	a node by the given name.
-      RETURNS: Node object, or NULL if not found.
-    */
-    public function FindNode($sName) {
-	if (!is_string($sName)) {
-	    throw new exception("Internal Error: Searching for node name that is type '".gettype($sName)."' instead of a string.");
-	}
-	if ($sName == $this->Name()) {
-	    return $this;
-	} elseif (!is_array($this->arSubs)) {
-	    return NULL;
-	} elseif (array_key_exists($sName,$this->arSubs)) {
-	    return $this->arSubs[$sName];
-	} elseif (is_array($this->arSubs)) {
-	    foreach ($this->arSubs as $key => $node) {
-		$oFnd = $node->FindNode($sName);
-		if (is_object($oFnd)) {
-		    return $oFnd;
-		}
-	    }
-	}
-	return NULL;
-    } /*
-    public function FindNode_debug($sName) {
-	if ($sName == $this->Name()) {
-	    return $this;
-	} elseif (array_key_exists($sName,$this->arSubs)) {
-	    return $this->arSubs[$sName];
-	} elseif (is_array($this->arSubs)) {
-	    foreach ($this->arSubs as $key => $node) {
-		$oFnd = $node->FindNode($sName);
-		if (is_object($oFnd)) {
-		    return $oFnd;
-		}
-	    }
-	}
-	return NULL;
-    } */
-    public function HasNodes() {
-	if (isset($this->arSubs)) {
-	    return (count($this->arSubs) > 0);
-	} else {
-	    return FALSE;
-	}
-    }
-    // PARENT NODE basic functions
     public function Parent(clsTreeNode $iNode=NULL) {
 	if (!is_null($iNode)) {
 	    $this->objParent = $iNode;
@@ -123,38 +68,31 @@ class clsTreeNode {
 	    return $this;
 	}
     }
-    // more subnode functions and actions
-    public function NodeAdd(clsTreeNode $iNode) {
-	$strName = $iNode->Name();
-	if (is_null($strName)) {
-	    $this->arSubs[] = $iNode;
+
+    // -- PARENT NODE -- //
+    // ++ SUBNODES -- LOOKUP/ADDING ++ //
+
+    public function HasNodes() {
+	if (isset($this->arSubs)) {
+	    return (count($this->arSubs) > 0);
 	} else {
-	    $this->arSubs[$strName] = $iNode;
+	    return FALSE;
 	}
-	$iNode->Parent($this);
+    }
+    /*----
+      ACTION: Retrieve the first sub-node.
+      PURPOSE: Mainly useful when there is exactly one
+	sub-node which we expect to exist.
+    */
+    public function FirstNode() {
+	$oNode = reset($this->arSubs);	// get the first node
+	return $oNode;			// return it
     }
     public function Exists($iName) {
 	if (isset($this->arSubs[$iName])) {
 	    return isset($this->arSubs[$iName]);
 	} else {
 	    return FALSE;
-	}
-    }
-    public function Delete($sName=NULL) {
-	if (is_null($sName)) {
-	    // deleting self from parent
-	    $sName = $this->Name();
-	    if ($this->HasParent()) {
-		$this->Parent()->Delete($sName);
-	    } else {
-		echo 'Trying to delete node "'.$sName.'":';
-		throw new exception('Cannot delete root node this way.');
-	    }
-	} else {
-	    // deleting named sub-item (this is where it actually happens)
-	    if (isset($this->arSubs[$sName])) {
-		unset($this->arSubs[$sName]);
-	    }
 	}
     }
     public function Loaded($iName=NULL) {
@@ -187,48 +125,88 @@ class clsTreeNode {
 	    }
 	}
     }
-    public function Spawn() {
-	$obj = new clsTreeNode();
-	return $obj;
-    }
-    public function Value($iVal=NULL) {
-	if (!is_null($iVal)) {
-	    if (!is_string($iVal)) {
-		echo 'Trying to set node value using a <b>'.get_class($iVal).'</b> object instead of a string.<br>';
-		throw new exception('Internal error');
-	    }
-	    $this->vVal = $iVal;
+    // TODO: the names of Node() and NodeAdd() should be swapped
+    //	Maybe as an interim step, rename Node() to AddNode()
+    public function Node($iName, clsTreeNode $iNode=NULL) {
+	if (!is_null($iNode)) {
+	    $iNode->Name($iName);
+	    $this->NodeAdd($iNode);
 	}
-	return $this->vVal;
+	if (isset($this->arSubs[$iName])) {
+	    return $this->arSubs[$iName];
+	} else {
+	    return NULL;
+	}
+    }
+    public function NodeAdd(clsTreeNode $iNode) {
+	$strName = $iNode->Name();
+	if (is_null($strName)) {
+	    $this->arSubs[] = $iNode;
+	} else {
+	    $this->arSubs[$strName] = $iNode;
+	}
+	$iNode->Parent($this);
+    }
+    public function Nodes(array $iNodes=NULL) {
+	if (is_array($iNodes)) {
+	    $this->arSubs = $iNodes;
+
+	    foreach ($iNodes as $name => $node) {
+		$node->Parent($this);
+	    }
+	}
+	return $this->arSubs;
     }
     /*----
-      FUTURE: For consistency, should probably be renamed NodeValue()
+      ACTION: searches from the current node downward to find
+	a node by the given name.
+      RETURNS: Node object, or NULL if not found.
     */
-/* 2011-12-25 who uses this?
-    public function SubValue($iName,$iVal=NULL) {
-	if (!is_null($iVal)) {
-	    $obj = $this->Spawn();
-	    $obj->Value($iVal);
-	} else {
-	    $obj = NULL;
+    public function FindNode($sName) {
+	if (!is_string($sName)) {
+	    throw new exception("Internal Error: Searching for node name that is type '".gettype($sName)."' instead of a string.");
 	}
-	return $this->Node($iName,$obj)->Value();
+	if ($sName == $this->Name()) {
+	    return $this;
+	} elseif (!is_array($this->arSubs)) {
+	    return NULL;
+	} elseif (array_key_exists($sName,$this->arSubs)) {
+	    return $this->arSubs[$sName];
+	} elseif (is_array($this->arSubs)) {
+	    foreach ($this->arSubs as $key => $node) {
+		$oFnd = $node->FindNode($sName);
+		if (is_object($oFnd)) {
+		    return $oFnd;
+		}
+	    }
+	}
+	return NULL;
     }
-*/
-    public function AccessCount() {
-	return $this->intRefs;
-    }
-    public function IncCount() {
-	$this->intRefs++;
-    }
-    public function ResetCount() {
-	$this->intRefs = 0;
-	if ($this->HasNodes()) {
-	    foreach ($this->Nodes() as $name => $node) {
-		$node->ResetCount();
+
+    // -- SUBNODES -- LOOKUP/ADDING -- //
+    // ++ SUBNODES -- OTHER ACTION ++ //
+
+    public function Delete($sName=NULL) {
+	if (is_null($sName)) {
+	    // deleting self from parent
+	    $sName = $this->Name();
+	    if ($this->HasParent()) {
+		$this->Parent()->Delete($sName);
+	    } else {
+		echo 'Trying to delete node "'.$sName.'":';
+		throw new exception('Cannot delete root node this way.');
+	    }
+	} else {
+	    // deleting named sub-item (this is where it actually happens)
+	    if (isset($this->arSubs[$sName])) {
+		unset($this->arSubs[$sName]);
 	    }
 	}
     }
+
+    // -- SUBNODES -- OTHER ACTION -- //
+    // ++ DEBUGGING ++ //
+
     public function DumpHTML() {
 	$out = NULL;
 	if ($this->intRefs > 0) {
@@ -262,4 +240,40 @@ class clsTreeNode {
 	}
 	return $out;
     }
+
+    // -- DEBUGGING -- //
+    // ++ DEPRECATED ++ //
+
+    // ...until their use can be documented, at least.
+
+    /*----
+      FUTURE: For consistency, should probably be renamed NodeValue()
+    */
+/* 2011-12-25 who uses this?
+    public function SubValue($iName,$iVal=NULL) {
+	if (!is_null($iVal)) {
+	    $obj = $this->Spawn();
+	    $obj->Value($iVal);
+	} else {
+	    $obj = NULL;
+	}
+	return $this->Node($iName,$obj)->Value();
+    }
+*/
+    public function AccessCount() {
+	return $this->intRefs;
+    }
+    public function IncCount() {
+	$this->intRefs++;
+    }
+    public function ResetCount() {
+	$this->intRefs = 0;
+	if ($this->HasNodes()) {
+	    foreach ($this->Nodes() as $name => $node) {
+		$node->ResetCount();
+	    }
+	}
+    }
+
+    // -- DEPRECATED -- //
 }
